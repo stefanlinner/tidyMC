893edfe9aa35e0f8b8258d59a9da127c084093e9
stefanlinner <stefan.linner97@gmail.com>
2022-08-14 16:57:45 +0200

    Rework simulation worker function

--- First_function.R
+++ First_function.R
@@ -8,13 +8,16 @@ library(codetools)
 library(rlang)
 library(MonteCarlo)
 library(rlang)
+library(checkmate)
+library(magrittr)
+library(stringr)
 
 # Montecarlo function
 
 parallelly::availableCores()
 
 
-cores_number <- 6
+cores_number <- 4
 repetitions <- 10
 
 test_func <- function(param = 0.1, n = 100, x1 = 1, x2 = 2){
@@ -24,7 +27,7 @@ test_func <- function(param = 0.1, n = 100, x1 = 1, x2 = 2){
   stat_2 <- var(data)
   
   if (x2 == 5){
-    stop("error message")
+    stop("x2 can't be 5!")
   }
   
   return(list(stat, stat_2))
@@ -32,125 +35,235 @@ test_func <- function(param = 0.1, n = 100, x1 = 1, x2 = 2){
 
 
 param_list <- list(n = 10, param = seq(from = 0, to = 1, by = 0.5),
-                   x1 = 1:2, x2 = 5)
+                   x1 = 1:2, x2 = 2)
 
-MCMC_func <- function(test_func, repetitions, param_list, packages,
-                      cores_number, simple = FALSE){
-  plan(multisession, workers = cores_number) 
-  # Order parameter names to match function order
-  func_argnames <- names(as.list(args(test_func)))
-  func_argnames <- func_argnames[-which(func_argnames == "")]
-  func_argnames_equal <- paste(func_argnames, " = ", sep = "") 
-  param_names <- names(param_list) 
-  param_list <- param_list[order(match(func_argnames, param_names))]
-  param_names <- names(param_list) 
-  
-  
-  # Create grid containers, maybe useless if we just extract it from the function
-  n_param<-length(param_list)                                                       
-  dim_vec<-numeric(n_param) 
-  for(i in 1:n_param){
-    dim_vec[i]<-length(param_list[[i]])
-  } 
-  containers <- character(n_param)                                              
-  for(i in 1:n_param){
-    (assign(paste(param_names[i],"_grid",sep=""),param_list[[i]])) # create grid for each parameter
-    containers[i] <-  paste(param_names[i],"_grid",sep="")
-  }
-  
-  # Create grid tibble
-  containers <- paste(containers, collapse = ",", sep = "")
-  aux <- eval(parse(text = paste("expand.grid(", containers, ");", sep = "")))
-  aux <- as_tibble(aux)
-  colnames(aux) <- param_names
-  grid_size <- nrow(aux)
-  
-  
-  
-  
-  
-  # Aux but for the number of repetitions could be improved
-  aux2 <- future_map_dfr(seq_len(repetitions), function(x) aux)
-  
-  # Nice names for the parameters
-  nice_names <- eval(parse(text = paste("data.frame(",
-                                        paste(rep("numeric(grid_size*repetitions)", n_param),
-                                              collapse = ","),
-                                        ")", sep = "", collapse = "")))
-  for (i in 1:n_param){
-    nice_names[, i] <- paste(colnames(aux2)[i], "=",
-                             as.character(unlist(aux2[,i], use.names = F)),
-                             sep = "")
-  }
-  nice_names <- eval(parse(text = paste("paste(",
-                                        paste("unlist(nice_names[,",
-                                              1:n_param, "]), ", sep = "",
-                                              collapse = ""),
-                                        "sep = \", \" )", sep = "")))
-  
-  # New function based on test_func that gives us the error message with the
-  # function call
-  deparsed_function <- deparse(test_func)
-  
-  test_func_2 <- eval(parse(text = paste(paste(deparsed_function[1:2], collapse = ""), "\n",
-                                         "cl <- paste(func_argnames_equal,
-      eval(parse(text  = paste(\"c(\", paste(func_argnames, sep = \"\", collapse = \", \"), \")\",
-                               sep = \"\", collapse = \"\"))),
-      sep = \"\", collapse = \", \")",
-                                         "\n",
-                                         paste("tryCatch({out <- test_func(", 
-                                               paste(func_argnames, func_argnames, sep = "=", collapse = ", "),
-                                               ")", sep = "", collapse = ""), "}, ", "\n", "error  ={ ",
-                                         paste("function(e) stop(paste(\"\nFunction error: \", unlist(rlang::catch_cnd(test_func(", 
-                                               paste(func_argnames, func_argnames, sep = "=", collapse = ", "),
-                                               ")))[[1]], \"\n At the parameters: \",  cl, collapse = \"\", sep = \"\"))", sep = "", collapse = ""), "});", "\n",
-                                         "return(out)}", sep = "",
-                                         collapse = "")))
-  
-  # Check the number of results and catch errors early
-  num_res <- length(unlist(future_pmap(.l = aux2[c(1,floor(nrow(aux2)*0.5), nrow(aux2)),],
-                                       .f = test_func_2,  
-                                       .options = furrr_options(seed = TRUE))[[1]]))
-  
-  # Results
-  results_list <- future_pmap(.l = aux2, .f = test_func_2, 
-                              .options = furrr_options(seed = TRUE))
-  
-  
-  
-  # Extract results
-  
-  if (simple == TRUE){
+future_mc <- 
+  function(
+    test_func,
+    repetitions,
+    param_list,
+    packages = "test",
+    parallelisation_plan = NULL, 
+    simple = FALSE
+    # ,test = TRUE
+  ){
     
-    out <- eval(parse(text = paste("data.frame(", 
-                                   paste("res_", 1:num_res, "=",
-                                         rep("numeric(grid_size*repetitions)",
-                                             num_res),
-                                         sep = "", collapse = ", "), ")", sep = "")))
+    # Asserting inputs
+    checkmate::assert_function(test_func, args = names(param_list))
+    checkmate::assert_int(repetitions, lower = 1)
+    checkmate::assert_list(param_list)
+    purrr::walk(seq_along(param_list), checkmate::assert_vector)
+    checkmate::assert_character(packages)
+    checkmate::assert_list(parallelisation_plan, null.ok = TRUE)
+    checkmate::assert_logical(simple, len = 1)
     
-    for (i in 1:length(results_list)){
-      for (j in 1:num_res){
-        out[i,j] <- results_list[[i]][j]
-      }
+    if(is.null(parallelisation_plan)){
+      parallelisation_plan <- 
+        list(
+          strategy = multisession, 
+          substitute = TRUE,
+          .skip = FALSE,
+          .call = TRUE,
+          .cleanup = TRUE, 
+          .init = TRUE
+        )
     }
-    out <- cbind(param = nice_names, out)
-  } else if (simple == FALSE){
-    out <- tibble(params =  nice_names,
-                  results = as_tibble_col(results_list))
+    
+    do.call(plan, parallelisation_plan)
+    # plan(multisession)
+    
+    # Order parameter names to match function order
+    func_argnames <- formalArgs(test_func)
+    # func_argnames <- names(as.list(args(test_func)))
+    # func_argnames <- func_argnames[-which(func_argnames == "")]
+    # func_argnames_equal <- paste(func_argnames, " = ", sep = "")
+    param_names <- names(param_list)
+    param_list <- param_list[order(match(func_argnames, param_names))]
+    param_names <- names(param_list)
+    
+    
+    # Create grid containers, maybe useless if we just extract it from the function
+    # n_param<-length(param_list)
+    # dim_vec<-numeric(n_param) 
+    # for(i in seq_along(param_list)){
+    #   dim_vec[i]<-length(param_list[[i]])
+    # } 
+    # containers <- character(n_param)
+    # for(i in seq_along(param_list)){
+    #   (assign(paste(param_names[i],"_grid",sep=""),param_list[[i]])) # create grid for each parameter
+    #   containers[i] <-  paste(param_names[i],"_grid",sep="")
+    # }
+    
+    # Create grid tibble
+    # containers <- paste(containers, collapse = ",", sep = "")
+    # aux <- eval(parse(text = paste("expand.grid(", containers, ");", sep = "")))
+    # colnames(aux) <- param_names
+    # grid_size <- nrow(aux)
+    
+    
+    
+    # Rewriting above part
+    n_param <- length(param_list)
+    aux <- as_tibble(
+      expand.grid(param_list)
+    )
+    grid_size <- nrow(aux)
+    
+    
+    
+    
+    # Aux but for the number of repetitions could be improved / future needed? 
+    aux2 <- purrr::map_dfr(seq_len(repetitions), function(x) aux)
+    
+    # Nice names for the parameters
+    nice_names <- 
+      purrr::map_chr(
+        seq_len(nrow(aux2)),
+        function(.x){
+          paste(
+            purrr::map_chr(
+              names(aux2), 
+              function(.z){
+                paste(
+                  .z, 
+                  "=",
+                  aux2[.x, .z], 
+                  sep = ""
+                )
+              }
+            ), 
+            collapse = ", "
+          )
+        })
+    
+    
+    # New function based on test_func that gives us the error message with the
+    # function call
+    
+    test_func_2 <- args(test_func)
+    body(test_func_2, envir = environment()) <- 
+      quote({
+        cl <- 
+          paste(
+            purrr::map_chr(
+              func_argnames, 
+              function(.x){
+                paste(.x, get(.x), sep = " = ")
+              }), 
+            collapse = ", "
+          )
+        
+        tryCatch(
+          {
+            out <- 
+              eval(
+                parse(
+                  text = 
+                    paste("test_func(", 
+                          paste(
+                            func_argnames, 
+                            func_argnames, 
+                            sep = "=", 
+                            collapse = ", "
+                          ),
+                          ")", 
+                          sep = ""
+                    )
+                )
+              )
+            return(out)
+          }, 
+          error  = {
+            function(e) 
+              #stop( I don't want early exit -> We prefer test-run (alternatively if test = F, then with stop!)
+              paste(
+                " \n Function error: ", eval(
+                  parse(
+                    text =
+                      paste("unlist(rlang::catch_cnd(test_func(", 
+                            paste(
+                              func_argnames, 
+                              func_argnames, 
+                              sep = "=", 
+                              collapse = ", "
+                            ),
+                            ")))[[1]]", sep = ""))),
+                " \n At the parameters: ",  cl, " \n" , collapse = "", sep = "")
+            #)
+          }
+        )
+      })
+    
+    # Run single test_iteration for each parameter setup
+    
+    message("Running single test-iteration for each parameter combination...")
+    
+    test_runs <- 
+      furrr::future_pmap(
+        .l = aux,
+        .f = test_func_2,
+        .progress = TRUE,
+        .options = furrr_options(seed = TRUE)
+      )
+    
+    test_runs_errors <- unlist(test_runs) %>% 
+      stringr::str_subset(pattern = "^ \n Function error")
+    
+    if(length(test_runs_errors != 0)){
+      stop(test_runs_errors)
+    } else {
+      message("Test-run successfull: No errors occurred!")
+    }
+    
+    # Check the number of results
+    num_res <- length(unlist(furrr::future_pmap(.l = aux2[1,],
+                                                .f = test_func_2,
+                                                .options = furrr_options(seed = TRUE))))
+    
+    
+    # Results
+    
+    message("Running whole simulation...")
+    
+    results_list <- 
+      furrr::future_pmap(
+        .l = aux2, 
+        .f = test_func_2, 
+        .progress = TRUE,
+        .options = furrr_options(seed = TRUE))
+    
+    message("Simulation was successfull!")
+    
+    # Extract results
+    
+    if(simple){
+      
+      res_names <- purrr::map_chr(seq_len(num_res), function(.x){
+        paste("res_", .x, sep = "")
+      })
+      
+      out <- purrr::map_dfr(results_list, function(.x){
+        res <- .x
+        names(res) <- res_names
+        res
+      })
+      
+      out <- as_tibble(cbind(param = nice_names, out))
+      
+    } else {
+      out <- tibble(params =  nice_names,
+                    results = as_tibble_col(results_list))
+    }
+    
+    plan("default")
+    return(out)
   }
-  
-  
-  
-  
-  plan("default")
-  return(out)
-}
 
-test <- MCMC_func(test_func = test_func, repetitions = 10, param_list = param_list,
-                  cores_number = 7, simple = FALSE)
 
-param_list <- list(n = 10, param = seq(from = 0, to = 1, by = 0.5),
-                   x1 = 1:2, x2 = 1:6)
+test <- future_mc(test_func = test_func, repetitions = 1000, param_list = param_list, simple = T)
+
+param_list <- list(n = 1000, param = seq(from = 0, to = 1, by = 0.5),
+                   x1 = 1:2, x2 = 1:4)
 
 
 
@@ -244,4 +357,11 @@ if(is.null(all_funcs_found)==FALSE){
     if(length(sort_out)>0){packages<-packages[-sort_out]}  # keep only those packages that are not automatically included because they are dependencies
   }
 }
+
+
+
+# summary functions -> list of function for each result in list
+# What to do with the furrr.options?
+# Nice output -> look in tidyverse package
+
 
