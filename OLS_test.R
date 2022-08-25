69ebb6ed29c71363606e8c54ac674dc0f30c8057
Ignacio Moreira <nacho0967@gmail.com>
2022-08-25 15:45:48 +0200

    OLS test

--- OLS_test.R
+++ OLS_test.R
@@ -18,13 +18,7 @@ ols_test <- function(b0, b1, b2, n,
 
 
   # Estimate OLS on the sample
-
-  estim <- lm(y ~ x1 + x2)
-
-
-  estim.summary <- summary(estim)
-
-  estim.summary$coefficients[,4]
+estim <- lm(y ~ x1 + x2)
 
   return(list(B0 = estim$coefficients[1],
               B1 = estim$coefficients[2],
@@ -36,12 +30,12 @@ ols_test <- function(b0, b1, b2, n,
 param_list <- list(b0 = 1, b1 = 4, b2 = 5, n = c(10, 100, 1000, 10000),
                    sigma2 = 2)
 
-test1 <- future_mc(fun = ols_test, repetitions = 10000, param_list = param_list)
+ols <- future_mc(fun = ols_test, repetitions = 10000, param_list = param_list)
 
-ols.latex <- tidy_mc_latex(summary(test1), repetitions_set = c(10, 10000))
+ols.latex <- tidy_mc_latex(summary(ols), repetitions_set = c(10, 10000))
 
 
-ols.plot_n10_b1 <- plot(x = test1, parameter_comb = list(n = 10))$B1
+ols.plot_n10_b1 <- plot(x = ols, parameter_comb = list(n = 10))$B1
 
 ols.plot_n10_b1 +
   ggplot2::theme_minimal() +
@@ -49,13 +43,75 @@ ols.plot_n10_b1 +
   ggplot2::labs(title = "Beta 1 results for 10 MC repetitions")
 
 
-ols.plot_n10_s2 <- plot(x = test1, which_setup = test1$nice_names[c(1,4)])$s2
+ols.plot_n10_s2 <- plot(x = test1, parameter_comb = list(n = 10))$s2
+
+ols.plot_n10_s2 +
+  ggplot2::theme_minimal() +
+  # ggplot2::ggtitle("Beta 1 results for 10 MC repetitions") +
+  ggplot2::labs(title = "s2 results for 10 MC repetitions")
+
+ols.plot_n10_b1 <- plot(x = test1, parameter_comb = list(n = 10000))$B1
+
+ols.plot_n10k_b1 +
+  ggplot2::theme_minimal() +
+  # ggplot2::ggtitle("Beta 1 results for 10 MC repetitions") +
+  ggplot2::labs(title = "Beta 1 results for 10000 MC repetitions")
 
 
-ols.plot_n10k <- plot(x = test1, which_setup = test1$nice_names[c(1,4)])[[2]]
+ols.plot_n10k_s2 <- plot(x = test1, parameter_comb = list(n = 10000))$s2
+
+ols.plot_n10k_s2 +
+  ggplot2::theme_minimal() +
+  # ggplot2::ggtitle("Beta 1 results for 10 MC repetitions") +
+  ggplot2::labs(title = "s2 results for 10000 MC repetitions")
 
 
-# Bootstrap function is not what is inteded but lets do it
+# Irrelevant variable
+
+
+ols_test <- function(b0, b1, b2, n,
+                     sigma2){
+  # Gen x independently
+  x1 <- rnorm(n = n, mean = 1, sd = 5)
+  x2 <- rnorm(n = n,  mean = 2, sd = 1)
+  # Generate error term
+  e <- rnorm(n, sd = sqrt(sigma2))
+
+  # Gen y dependently
+  y <- b0 + b1*x1 + b2*x2 + e
+
+
+  # Estimate OLS on the sample
+  estim <- lm(y ~ x1 + x2)
+
+  estim.summary <- summary(estim)
+
+
+  return(list(B2 = estim$coefficients[3],
+              Pval_B2 = ifelse(estim.summary$coefficients[3,4]<0.05, "Reject", "Not reject"),
+              s2 = var(estim$residuals)))
+}
+
+param_list <- list(b0 = 1, b1 = 5, b2 = 0, n = c(10, 100, 1000, 10000),
+                   sigma2 = 4)
+
+ols_irr <- future_mc(fun = ols_test, repetitions = 10000, param_list = param_list)
+
+ols_irr.summary <- summary(ols_irr)
+
+ols_irr.latex <- tidy_mc_latex(x)
+
+
+
+
+
+
+
+
+
+
+
+# Bootstrap function is not what is intended but lets do it
 
 
 b0 <- 2
